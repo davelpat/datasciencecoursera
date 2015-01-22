d846d02312d223cf94bde7c45ead1f65d4235ec5
davelpat <davelpat@gmail.com>
2015-01-21 23:38:18 -0800

    Initial signature for complete data

---
+++ complete.R
@@ -0,0 +1,13 @@
+complete <- function(directory, id = 1:332) {
+  ## 'directory' is the location of the CSV files
+  
+  ## 'id' is the set of monitor ID numbers
+  
+  ## Return a data frame of the form:
+  ## id nobs
+  ## 1  117
+  ## 2  1041
+  ## ...
+  ## where 'id' is the monitor ID number and 'nobs' is the
+  ## number of complete cases
+}
