76ef007c72a7d2093c0b14a9311c38a76db74ccc
davelpat <davelpat@gmail.com>
2015-02-01 22:00:50 -0800

    version of corr that processes files one at a time

---
+++ corr2.R
@@ -0,0 +1,31 @@
+corr <- function(directory, threshold = 0) {
+  ## 'directory' is the location of the CSV files
+  
+  ## 'threshold' is the number of completely observed observations
+  ## required to compute the correlation between
+  ## nitrate and sulfate; the default is 0
+  
+  ## Returns a numeric vector of correlations
+  
+  ## get the set of files
+  files <- list.files(directory, full.names = T)
+  ## mask to get only the data columns of interest
+  mask <- c("NULL", "numeric", "numeric", "NULL")
+  
+  correlations <- vector("numeric")
+  ## Process files one at a time to reduce memory requirements
+  for(fn in files){
+    # read in the pollutant data
+    dat <- read.csv(fn, colClasses=mask, nrows=4019)
+    # Find the complete cases
+    cases <- complete.cases(dat$nitrate, dat$sulfate)
+    # Does it meet the threshold?
+    if(sum(cases) > threshold){
+      # Run the correlation between complete cases of the pollutants and collect the result
+      correlations <- c(correlations, cor(dat$nitrate[cases], dat$sulfate[cases]))#, use="pairwise.complete.obs"))
+    }
+  }
+  
+  ## return the correlation vector
+  correlations
+}
