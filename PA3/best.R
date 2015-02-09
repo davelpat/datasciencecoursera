e851f30b06262a6bdf6de20349e092cdb0797911
davelpat <davelpat@gmail.com>
2015-02-08 18:42:45 -0800

    Updated comments for gen_outcome_classes

--- PA3/best.R
+++ PA3/best.R
@@ -7,7 +7,11 @@
 ## or "pneumonia".
 
 ## utility function to generate colClasses pattern
-## takes a list of class types and indices
+## Required parameter is a list of vectors, each named for the
+## desired class, of indices where that class is desired.
+## The rest of the vector is filled with "NULL".
+## Optional "len" parameter sets the length of the vector.
+## Default len = 46
 ## returns a colClasses character vector
 gen_outcome_classes <- function(non_null, len=46) {
   # generate the list of nulls as wide as the outcomes table
