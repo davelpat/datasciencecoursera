73f1e4b7a84a77e247ccfc58c78e65a592d87939
davelpat <davelpat@gmail.com>
2015-03-15 01:20:46 -0700

    reorganizing repo to reflect John Hopkins Data Science structure

---
+++ R_programming/PA1/complete.R
@@ -0,0 +1,28 @@
+complete <- function(directory, id = 1:332) {
+  ## 'directory' is the location of the CSV files
+  
+  ## 'id' is the set of monitor ID numbers
+  
+  ## Return a data frame of the form:
+  ## id nobs
+  ## 1  117
+  ## 2  1041
+  ## ...
+  ## where 'id' is the monitor ID number and 'nobs' is the
+  ## number of complete cases
+  
+  ## get the set of files
+  files <- sprintf("%s/%03d.csv", directory, id)
+  
+  ## Get a data frame of pollutant observations for each monitor
+  dat <- lapply(files, read.csv, colClasses=c("NULL", "numeric", "numeric", "NULL"), nrows=4019)
+  
+  ## For each monitor, count the number of complete observations
+  counts <- vector("integer")
+  for(i in seq_along(id)){
+    counts[i] <- sum(complete.cases(dat[[i]][1:2]))
+  }
+
+  ## return the correctly labeled data structure
+  data.frame(id=id, nobs=counts)
+}
---
+++ R_programming/PA1/corr.R
@@ -0,0 +1,28 @@
+corr <- function(directory, threshold = 0) {
+  ## 'directory' is the location of the CSV files
+  
+  ## 'threshold' is the number of completely observed observations
+  ## required to compute the correlation between
+  ## nitrate and sulfate; the default is 0
+  
+  ## Returns a numeric vector of correlations
+  ## set options(digits=4) before submitting to print the correct numbers
+  
+  ## get the set of files
+  files <- list.files(directory, full.names = T)
+  
+  ## Get a data frame of pollutant observations for each monitor
+  dat <- lapply(files, read.csv, colClasses=c("NULL", "numeric", "numeric", "NULL"), nrows=4019)
+  
+  ## For each monitor, count the number of complete observations
+  corrs <- vector("numeric")
+  for(i in 1:length(files)){
+    cnt <- sum(complete.cases(dat[[i]][1:2]))
+    if(cnt > threshold){
+      corrs <- c(corrs, cor(dat[[i]]$nitrate, dat[[i]]$sulfate, use="pairwise.complete.obs"))
+    }
+  }
+  
+  ## return the correlation vector
+  return(corrs)
+}
